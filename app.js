866df793ab611cfb721dcd94603db26387ae20a9
Lindsay Sack <lindsay.sack@gmail.com>
2018-04-19 11:54:48 -0400

    finished configuring express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require( 'express' );
+const volleyball = require('volleyball');
 const app = express(); // creates an instance of an express application
 
-
+app.use(volleyball);
+// app.use((req, res, next) => {
+//     console.log(req.method, req.path, res.statusCode);
+//     next();
+// })
+// app.use('/news/', (req, res, next) => {
+//     console.log('news special log');
+//     next();asdfsd
+// })
 app.get('/', (req, res, next) => {
     res.send("server listening"); 
 })
+app.get('/news', (req, res, next) => {
+    res.send("found the news!")
+})
+
 app.listen(3000);
